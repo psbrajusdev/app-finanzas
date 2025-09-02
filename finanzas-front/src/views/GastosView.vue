95d35c694c4e5bd25470aba3fa9add34d79cfe03
Pablo Brajus <psbrajus@gmail.com>
2025-09-02 07:28:07 -0300

    Creando gitignore

--- finanzas-front/src/views/GastosView.vue
+++ finanzas-front/src/views/GastosView.vue
@@ -2,5 +2,16 @@
 
 <template>
   <h1>Pagina de Gastos</h1>
+
+  <h2>Gastos Fijos</h2>
+  <ul>
+    <li>Internet</li>
+    <li>Luz</li>
+    <li>Gas</li>
+    <li>Seguro</li>
+    <li>Combustible</li>
+    <li>Patente</li>
+    <li>Comida</li>
+  </ul>
 </template>
 <style scoped></style>
